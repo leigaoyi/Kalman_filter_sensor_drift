88e51a5ce18b72545a4efa87bade549da0c60b97
Lei <lei@126.com>
2023-08-08 10:42:37 +0800

    better files structure

--- LabView_data_conversion.m
+++
@@ -1,9 +0,0 @@
-function [time,data_out] = LabView_data_conversion(data_in,fs)
-data_in = data_in';
-[m,n] = size(data_in);
-ADC_sample = zeros(1,m*n);
-for i = 1:m*n
-    ADC_sample(i) = data_in(i);
-end
-data_out = ADC_sample;
-time = (1:length(data_out))/fs;
